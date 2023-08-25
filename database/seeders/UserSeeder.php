9f75b61a94598bee44b2b3db20a6c0d731a46987
1986webdeveloper <1986webdeveloper@gmail.com>
2023-08-25 15:41:34 +0530

    added migration and seeder

---
+++ database/seeders/UserSeeder.php
@@ -0,0 +1,32 @@
+<?php
+
+namespace Database\Seeders;
+
+use Illuminate\Database\Seeder;
+use Illuminate\Support\Facades\Hash;
+
+class UserSeeder extends Seeder
+{
+    public function run()
+    {
+        \App\Models\User::truncate();
+        // Create default admin account
+        \App\Models\User::create([
+            'uuid'              => \Illuminate\Support\Str::uuid(),
+            'first_name'        => 'Admin',
+            'last_name'         => 'User',
+            'is_admin'          => true,
+            'email'             => 'admin@buckhill.co.uk',
+            'email_verified_at' => now(),
+            'password'          => Hash::make('admin'),
+            'avatar'            => null,
+            'address'           => '123 Admin Street',
+            'phone_number'      => '123-456-7890',
+            'is_marketing'      => false,
+            'created_at'        => now(),
+        ]);
+
+        // Create other users using factory
+        \App\Models\User::factory(9)->create();
+    }
+}
